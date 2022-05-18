fe3ab417e9587607c4181c113f1d7cd3bc969702
Mark Kennedy <kennedymark@hotmail.com>
2022-05-18 16:02:31 -0400

    started about section

--- portfolio/src/components/About.jsx
+++ portfolio/src/components/About.jsx
@@ -1,14 +1,17 @@
 import ProfilePicture from '../resources/IMG_9217.jpg'
+import Skills from './Skills'
 
 const About = () => {
   return (
     <div className="about">
       <div className="about-title">About</div>
       <div className="about-info">
-        <div className="about-info-text">I am a software developer.I am a software developer.I am a software developer.I am a software developer.I am a software developer.I am a software developer.I am a software developer.I am a software developer.</div>
+        <div className="about-info-text">
+          <p>I am a software developer.I am a software developer.I am a software developer.I am a software developer.I am a software developer.I am a software developer.I am a software developer.I am a software developer.</p>
+          <Skills />
+          </div>
         <div className="about-info-image">
           <img src={ProfilePicture} alt="Mark Kennedy" className="profile-picture"/>
-          {/* <div className="profile-picture"></div> */}
         </div>
       </div>
     </div>
---
+++ portfolio/src/components/Skills.jsx
@@ -0,0 +1,17 @@
+const Skills = () => {
+  return (
+    <div className="skills">
+      <ul className="skills-list">
+        <li><span>√ </span>React</li>
+        <li><span>√ </span>JavaScript</li>
+        <li><span>√ </span>HTML/CSS</li>
+        <li><span>√ </span>Node.js</li>
+        <li><span>√ </span>Express.js</li>
+        <li><span>√ </span>MongoDB</li>
+        <li><span>√ </span>PostgreSQL</li>
+      </ul>
+    </div>
+  )
+}
+
+export default Skills
