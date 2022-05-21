5d2e4ddb5435cb46d6305110d2effaf774181ca2
Mark Kennedy <kennedymark@hotmail.com>
2022-05-21 15:49:58 -0400

    email right side

--- src/App.js
+++ src/App.js
@@ -8,6 +8,9 @@ function App() {
   return (
     <div className="App">
       <Nav />
+      <div orientation="left" className="side-right">
+        <div className="test">kennedymark680@gmail.com</div>
+      </div>
       <Intro />
       <About />
       <Projects />
