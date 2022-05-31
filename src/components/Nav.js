d2108b2a26e012c9c10c6f40ecb7c62d16fc3525
Mark Kennedy <kennedymark@hotmail.com>
2022-05-31 15:57:32 -0400

    changed name

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,11 +5,17 @@ const Nav = () => {
     <div className="nav">
       <div>
         <ol>
-          <li><a href='#about'>About</a></li>
-          <li><a href='#projects'>Projects</a></li>
-          <li>Experience</li>
-          <li>Contact</li>
-          <li><a href={Resume} target='_blank'>Resume</a></li>
+          <li>
+            <a href="#about">About</a>
+          </li>
+          <li>
+            <a href="#projects">Projects</a>
+          </li>
+          <li>
+            <a href={Resume} target="_blank">
+              Resume
+            </a>
+          </li>
         </ol>
       </div>
     </div>
